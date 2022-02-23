8710f0ef9b3861b21747014c01b5eda4beefcdaf
Ryan Saunders <31867251+ryguy4@users.noreply.github.com>
2022-02-23 14:59:45 -0700

    Update TCP_2client_server.py
    Created functions to reduce code redundancy

--- TCP_2client_server.py
+++ TCP_2client_server.py
@@ -9,19 +9,50 @@
 #   send a message to the server that will be saved in a file.
 #
 ##############################################################################
-# Note: Take-2 header goes here
+# Changes made to my code for the Lab 1 Take-2:
+# - Reduced code redundancy by putting repeated code into a function.
+# - Slightly modified/corrected comments throughout the code to futher clarify what is happening and make things easier to read.
 #
 ##############################################################################
 
 import sys
 from socket import *
 
+
+def openFile(connection, clientNum):
+    """
+    Get a file name from the provided connection and try to open the file. Returns the open file once succesfully opened.
+    """
+    while 1:
+        try:
+            fileName = connection.recv(1024).decode('ascii')
+            file = open(f"{fileName}", "a")
+            print(f"File name received from client {clientNum}:  {fileName}")
+            connection.send("FOK".encode('ascii'))
+            return file                             
+        except IOError:
+            connection.send("ERROR".encode('ascii'))
+
+def receiveWriteMessage(connection, clientNum, file):
+    """
+    Gets a message from the server and writes it to the provided file unless the message is ".end". Returns 1 if a normal message was received, 0 if signal to close connection was received.
+    """
+    message = connection.recv(1024).decode('ascii')
+    print(f"Message from client {clientNum}:  {message}")
+    if message == ".end":
+        connection.send("OK".encode('ascii'))
+        return 0
+    else:
+        file.write(message)
+        file.write("\n")
+        connection.send("OK".encode('ascii'))
+        return 1
+
 if len(sys.argv) < 2:
     print('Usage: TCP_2client_client hostname port')
     sys.exit(1)
 
 serverPort = int(sys.argv[1])
-
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
@@ -30,65 +61,25 @@ print('The Server is listening ... ')
 print('Server waiting for two clients to connect ... ')
 
 try:
-    # Get connection to client 1
+    # Get connection to client 1 then open the file received from the client or return an error is unable to open
     connectionSocket1, addr1 = serverSocket.accept()
-    # Open file received from client or return error if unable to open
-    while True:
-        try:
-            fileName = connectionSocket1.recv(1024).decode('ascii')
-            f1 = open(f"{fileName}", "a")
-            print(f"File name received from client 1:  {fileName}")
-            connectionSocket1.send("FOK".encode('ascii'))
-            break                             
-        except IOError:
-            connectionSocket1.send("ERROR".encode('ascii'))
+    f1 = openFile(connectionSocket1, 1)
 
-    # Get connection to client 2
+    # Get connection to client 2 then open the file received from the client or return an error is unable to open
     print('Server waiting for another client to connect ... ')
     connectionSocket2, addr2 = serverSocket.accept()
-
-    # Open file received from client or return error if unable to open
-    while True:
-        try:
-            fileName = connectionSocket2.recv(1024).decode('ascii')
-            f2 = open(f"{fileName}", "a")
-            print(f"File name received from client 2:  {fileName}")
-            connectionSocket2.send("FOK".encode('ascii'))
-            break                             
-        except IOError:
-            connectionSocket2.send("ERROR".encode('ascii'))
+    f2 = openFile(connectionSocket2, 2)
 
     c1Status = 1
     c2Status = 1
     while 1:
-        # Check if client 1 has ended the connection yet. If it has not ended the connection, wait for a message and then add it to
-        # the appropriate file.
+        # Check if a client has ended the connection yet. If it has not ended the connection, wait for a message and then add it to the appropriate file.
         if c1Status:
-            message = connectionSocket1.recv(1024).decode('ascii')
-            if message == ".end":
-                print(f"Message from client 2:  {message}")
-                c1Status = 0
-                connectionSocket1.send("OK".encode('ascii'))
-            else:
-                print(f"Message from client 1:  {message}")
-                f1.write(message)
-                f1.write("\n")
-                connectionSocket1.send("OK".encode('ascii'))
-        # Check if client 2 has ended the connection yet. If it has not ended the connection, wait for a message and then add it to
-        # the appropriate file.
+            c1Status = receiveWriteMessage(connectionSocket1, 1, f1)
         if c2Status:
-            message = connectionSocket2.recv(1024).decode('ascii')
-            if message == ".end":
-                print(f"Message from client 2:  {message}")
-                c2Status = 0
-                connectionSocket2.send("OK".encode('ascii'))
-            else:
-                print(f"Message from client 2:  {message}")
-                f2.write(message)
-                f2.write("\n")
-                connectionSocket2.send("OK".encode('ascii'))
+            c2Status = receiveWriteMessage(connectionSocket2, 2, f2)
 
-        # If both clients have ended either connection, then stop receiving messages
+        # If both clients have ended their connection, then stop receiving messages
         if not c1Status and not c2Status:
             break
 
